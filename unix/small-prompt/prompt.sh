768cfc9cacbfb3a0274c6d3ff3214e1222ae7ef8
Microeinstein <microeinstein@inventati.org>
2023-07-10 09:07:30 +0200

    [small-prompt] rewrite bash script

--- unix/small-prompt/prompt.sh
+++ unix/small-prompt/prompt.sh
@@ -1,114 +1,142 @@
-__creset="\[\e[0m\]"
-__ccl="\[\e[K\]"
-__cgray="${__creset}\[\e[37m\]"
-__cwhite="${__creset}\[\e[97m\]"
-__cred="${__creset}\[\e[91m\]"
-__cdarkred="${__creset}\[\e[31m\]"
-__cgreen="${__creset}\[\e[92m\]"
-__cdarkgreen="${__creset}\[\e[32m\]"
-__cblue="${__creset}\[\e[94m\]"
-#
-#int color256rainbow[] = {
-#	196, 202, 208, 214, 220, 226, 190, 154, 118, 82,  46,  47,  48,  49,  50,
-#	51,  45,  39,  33,  27,  21,  57,  93,  129, 165, 201, 200, 199, 198, 197
-#};
-#int color256length = 30;
-__crainbow=(
-    196  202  208  214  220  226  190  154  118  82
-    46   47   48   49   50   51   45   39   33   27
-    21   57   93   129  165  201  200  199  198  197
-)
-__crainbowlight=(
-    160  166  172  178  142  106  70   71   72   73
-    74   68   62   56   92   128  164  163  162  161
-)
-__crainbowlen=${#__crainbow[@]}
-if [[ "$(id -u)" == "0" ]]; then
-    __PS1_CLIGHT="${__cred}"
-    __PS1_CDARK="${__cdarkred}"
-    __crainbowi=-1
-else
-    __PS1_CLIGHT="${__cgreen}"
-    __PS1_CDARK="${__cdarkgreen}"
-    __crainbowi=8
-fi
-
-
-_adapt_prompt() (
-    username="\u"
-    hostname="\h"
-    shelltype="\\$"
-    term_len=$(tput cols)
-    currpath="\w"
-    currpath="${currpath@P}"
-    currpath="${currpath#/mnt/*/}"
-    currdir="\W"
-    if (( (${#currpath}+${#USER}+4) > (term_len/2) )) \
-    || [[ "$KATE_PID" != "" ]] \
-    || [[ "$TERM_PROGRAM" == "vscode" ]]; then
-        _smpl
+setup_PS1() {
+    local -A C=(
+        [r]='0'    # reset
+        [b]='1'    # bold
+        [f]='2'    # faint
+        [nb]='22'  # not bold/faint
+        # fg dark colors
+        [fdred]='31'
+        [fdgre]='32'
+        [fdwhi]='37'
+        [fc256]='38;5;%s'
+        # fg light colors
+        [flbla]='90'
+        [flred]='91'
+        [flgre]='92'
+        [flblu]='94'
+        [flwhi]='97'
+    )
+    local -A B=(
+        [user]='\u'
+        [host]='\h'
+        [euid]='\$'
+        [path]='\w'
+        [cdir]='\W'
+    )
+    
+    CSI() {
+        #  r b  ->  C[r];C[b]
+        local a  IFS=';'  t=()
+        for a in "$@"; do t+=("${C[$a]}"); done
+        printf '\x01\e[%sm\x02' "${t[*]}"
+    }
+    CSI2() {
+        #  b fmt 5  ->  C[b];C[fmt]%5
+        local  t=("$@")
+        printf  "$(CSI "${t[@]:0: $(( ${#t[@]} - 1))}")"  "${t[@]: -1}"
+    }
+    
+    
+    local rever=0  light=0  simple=0
+    
+    # KONSOLE_VERSION
+    [[ -v XTERM_VERSION ]]                                       && rever=1
+    [[ -v TERM_PROGRAM || -v TERMINAL_EMULATOR || -v KATE_PID ]] && simple=1
+    [[ -v TERM_PROGRAM || -v TERMINAL_EMULATOR ]]                && light=1
+    
+    if ((rever)); then
+        C[fdwhi]='30'
+        C[flwhi]='90'
+        printf '\e[?5h\e[?30h'  # reverse (all) video + show scrollbar
+    fi
+    if ((light)); then
+        __RAINBOW=(
+            160  166  172  178  142  106  70   71   72   73
+            74   68   62   56   92   128  164  163  162  161
+        )
     else
-        adapt_prompt_choice
+        __RAINBOW=(
+            196  202  208  214  220  226  190  154  118  82
+            46   47   48   49   50   51   45   39   33   27
+            21   57   93   129  165  201  200  199  198  197
+        )
     fi
-    return $?
-)
-adapt_prompt() {
-    #echo "[adapt_prompt] called by bash" >&2
-    if [[ "$__PS1" != "" ]] && [[ "$PS1" != "$__PS1" ]]; then
-        return
+    __RNBL=${#__RAINBOW[@]}
+    
+    
+    if (($(id -u))); then  # user
+        C[flusr]="${C[flgre]}"
+        C[fdusr]="${C[fdgre]}"
+        ((__RNBI= light ? 6 : 8))
+    else                   # root
+        C[flusr]="${C[flred]}"
+        C[fdusr]="${C[fdred]}"
+        __RNBI=-1
     fi
-    ((__crainbowi = (__crainbowi + ${1:-0}) % __crainbowlen))
-    local newps1
-    if ! newps1=$(_adapt_prompt); then
-        return
-    fi
-    export PS1="$newps1"
-    export __PS1="$PS1"
-    #trap - SIGWINCH
-    #kill -s SIGWINCH "$$"
-    #trap 'adapt_prompt' SIGWINCH
-}
-_smpl() {
-    __PS1_CRLIGHT="${__creset}\[\e[1;38;5;${__crainbow[${__crainbowi}]}m\]"
-    __PS1_CRDARK="${__creset}\[\e[38;5;${__crainbow[${__crainbowi}]}m\]"
-    echo "${__cgray}${currdir} ${__PS1_CRLIGHT}${shelltype}${__PS1_CRDARK}>${__cwhite} "
-}
-_cmplx() {
-    # export PS0="${cgray}"$(yes "⎺" | head -n $term_len | tr -d '\n')"${creset}\n"
-    # export PS1="${cblue}[${cselect}${username}${cblue}@${cgray}${hostname}${cblue}]${cwhite}: ${cgray}${currpath}${cblue}>${cselect}${shelltype}${creset} "
-    __PS1_CRLIGHT="${__creset}\[\e[1;38;5;${__crainbow[${__crainbowi}]}m\]"
-    echo "${__PS1_CRLIGHT}${username}${__cwhite}) ${__cgray}${currpath} ${__PS1_CLIGHT}${shelltype}${__PS1_CDARK}>${__cwhite} "
-}
-smpl() {
-    adapt_prompt_choice() {
-        _smpl
+    
+    
+    __ABBR="$(CSI f)*$(CSI nb)"
+    __OLDPWD=()  # cache
+    __MYPWD() {
+        local d="$PWD"
+        [[ "${__OLDPWD[0]}" == "$d" ]] && {
+            echo "${__OLDPWD[1]}"
+            return
+        }
+        __OLDPWD[0]="$d"
+        
+        # reduce known
+        case "$d" in
+            "$HOME")        d='~'        ;;
+            '/mnt/files/'*) d="${d: 11}" ;;
+        esac
+        ((${#d} < 11)) && { echo "$d"; return; }
+        
+        # reduce long
+        local -n r=BASH_REMATCH
+        local n
+        for n in {1..20}; do
+            [[ "$d" =~ /([^/*]{16,}) ]] || break
+            d="${d//"${r[0]}"/"/${r[1]:0:7}*${r[1]: -7}"}"
+        done
+        
+        # cut
+        local cols="$(tput cols)"
+        local cut=$((${#d} - (cols / 3 - ${#USER} - 4)))
+        ((cut = cut < 2 ? 0 : cut))
+        ((cut)) && d="*${d: $cut}"
+        
+        # pretty
+        d="${d//'*'/"$__ABBR"}"
+        __OLDPWD[1]="$d"
+        echo "$d"
     }
-}
-cmplx() {
-    adapt_prompt_choice() {
-        _cmplx
-    }
+    
+    # ●
+    shopt -s promptvars
+    local IFS=''
+    local anim='${__RAINBOW[ $((__RNBI = (__RNBI+1) % __RNBL)) ]}'
+    if ((simple)); then
+        local ps1=(
+            $(CSI r)                 '$(__MYPWD) '
+            $(CSI2 b fc256 "$anim")  ${B[euid]}
+            $(CSI nb)                '>'
+            $(CSI r)                 ' '
+        )
+    else
+        local ps1=(
+            $(CSI2 r b fc256 "$anim")  ${B[user]}
+            $(CSI r)                   ') $(__MYPWD) '
+            $(CSI flusr)               ${B[euid]}
+            $(CSI fdusr)               '>'
+            $(CSI r)                   ' '
+        )
+    fi
+    PS0="$(CSI r)"
+    PS1="${ps1[*]}"
+    
+    
+    unset CSI CSI2
 }
 
-
-unset __PS1
-if [[ "$XTERM_VERSION" != "" ]]; then
-    __cgray="${__creset}\[\e[30m\]"
-    __cwhite="${__creset}\[\e[90m\]"
-    printf "\e[?5h\e[?30h"
-fi
-if [[ "$TERM_PROGRAM" == "vscode" ]] || [[ "$TERMINAL_EMULATOR" == "JetBrains-JediTerm" ]]; then
-    __cgray="${__creset}"
-    __cwhite="${__creset}"
-    unset __crainbow
-    __crainbow=("${__crainbowlight[@]}")
-    __crainbowlen=${#__crainbowlight[@]}
-    __crainbowi=6
-fi
-# printf "\e[6 q"
-cmplx
-# trap 'adapt_prompt' SIGWINCH
-# trap adapt_prompt DEBUG
-_prompt_my() { adapt_prompt 1; }
-PROMPT_COMMAND+=('_prompt_my')
-export PS0="${__creset}"
+setup_PS1
